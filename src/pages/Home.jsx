21507cff79d6da53f16abc6345b6612e799edbae
Mehedi0101 <mehedi29121998@gmail.com>
2023-10-15 21:21:56 +0600

    features added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Home/Banner";
+import Features from "../components/Home/Features";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Features></Features>
         </div>
     );
 };
