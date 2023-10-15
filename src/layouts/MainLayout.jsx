ba9c08554364229e71dc352187323f4e9dc3c05b
Mehedi0101 <mehedi29121998@gmail.com>
2023-10-15 19:43:30 +0600

    header added

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/MainLayout/Header";
+
+const MainLayout = () => {
+    return (
+        <div className="max-w-screen-2xl mx-auto">
+            <Header></Header>
+        </div>
+    );
+};
+
+export default MainLayout;
